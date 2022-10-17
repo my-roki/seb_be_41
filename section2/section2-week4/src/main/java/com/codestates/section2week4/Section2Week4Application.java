2b5baa844b7a4e44682ff94c9714dea9c3cea72c
my-roki <anthonychangrok@gmail.com>
2022-10-17 16:50:11 +0900

    Dependency Injection - Add @ComponentScan.Filter

--- section2/section2-week4/src/main/java/com/codestates/section2week4/Section2Week4Application.java
+++ section2/section2-week4/src/main/java/com/codestates/section2week4/Section2Week4Application.java
@@ -2,8 +2,12 @@ package com.codestates.section2week4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
+@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
 public class Section2Week4Application {
 
 	public static void main(String[] args) {
