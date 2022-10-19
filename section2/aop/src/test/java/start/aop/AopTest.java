04317adce2e7236c3d527eff850abcdf3da764cc
my-roki <anthonychangrok@gmail.com>
2022-10-19 16:11:48 +0900

    Aspect Oriented Programming - Add advise

--- section2/aop/src/test/java/start/aop/AopTest.java
+++ section2/aop/src/test/java/start/aop/AopTest.java
@@ -8,7 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 //import start.aop.order.Aspect1;
-import start.aop.order.Aspect2;
+//import start.aop.order.Aspect2;
+import start.aop.order.Aspect3;
 import start.aop.order.OrderRepository;
 import start.aop.order.OrderService;
 
@@ -17,7 +18,8 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 @Slf4j
 @SpringBootTest
 //@Import(Aspect2.class)  // Aspect1를 만들고 AOP로 사용하려면 스프링 빈으로 등록해야합니다.
-@Import(Aspect2.class)
+//@Import(Aspect2.class)
+@Import(Aspect3.class)
 public class AopTest {
     @Autowired
     OrderService orderService;
