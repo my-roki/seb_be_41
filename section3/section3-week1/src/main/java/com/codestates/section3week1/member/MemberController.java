b71abb3a8ec43a0f92b4c4fcba4f17f2515b7b41
my-roki <anthonychangrok@gmail.com>
2022-10-20 14:23:43 +0900

    [Spring MVC] API 계층: Controller - Controller class and struct

---
+++ section3/section3-week1/src/main/java/com/codestates/section3week1/member/MemberController.java
@@ -0,0 +1,9 @@
+package com.codestates.section3week1.member;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/members")
+public class MemberController {
+}
---
+++ section3/section3-week1/src/main/java/com/codestates/section3week1/order/OrderController.java
@@ -0,0 +1,9 @@
+package com.codestates.section3week1.order;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/orders")
+public class OrderController {
+}
