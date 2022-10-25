f49c18e8248f19fcf753d7e64244c6e1b12275ff
my-roki <anthonychangrok@gmail.com>
2022-10-25 16:40:31 +0900

    [Spring MVC] 예외 처리: Spring MVC에서의 예외 처리 - Exception handling using @RestControllerAdvice

--- section3/week2-exception-handle/src/main/java/com/codestates/member/controller/MemberController.java
+++ section3/week2-exception-handle/src/main/java/com/codestates/member/controller/MemberController.java
@@ -6,19 +6,14 @@ import com.codestates.member.dto.MemberResponseDto;
 import com.codestates.member.entity.Member;
 import com.codestates.member.mapper.MemberMapper;
 import com.codestates.member.service.MemberService;
-import com.codestates.response.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
 import javax.validation.constraints.Positive;
 import java.util.List;
-import java.util.stream.Collectors;
 
 
 /**
@@ -81,30 +76,5 @@ public class MemberController {
         memberService.deleteMember(memberId);
 
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity handlerException(MethodArgumentNotValidException e) {
-        List<FieldError> fieldErrors = e.getBindingResult().getFieldErrors();
-
-        List<ErrorResponse.FieldError> errors = fieldErrors.stream()
-                .map(error -> new ErrorResponse.FieldError(
-                        error.getField(),
-                        error.getRejectedValue(),
-                        error.getDefaultMessage()))
-                .collect(Collectors.toList());
-
-        return new ResponseEntity<>(new ErrorResponse(errors), HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity handleException(ConstraintViolationException e) {
-        /**
-         * - ConstraintViolationException 클래스는 getBindingResult().getFieldErrors()
-         * 와 같이 에러 정보를 얻을 수 없다.
-         * - MethodArgumentNotValidException과 다르게 또 다른 방식으로 처리가 필요.
-         */
-
-        return new ResponseEntity(HttpStatus.BAD_REQUEST);
     }
 }
--- section3/week2-exception-handle/src/main/java/com/codestates/response/ErrorResponse.java
+++ section3/week2-exception-handle/src/main/java/com/codestates/response/ErrorResponse.java
@@ -1,20 +1,77 @@
 package com.codestates.response;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.validation.BindingResult;
 
+import javax.validation.ConstraintViolation;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Getter
-@AllArgsConstructor
 public class ErrorResponse {
-    private List<FieldError> fieldErrors;
+    private List<FieldError> fieldErrors;  // MethodArgumentNotValidException 으로부터 발생하는 에러 정보를 담는 멤버 변수
+    private List<ConstraintViolationError> constraintViolationErrors;  // ConstraintViolationException 으로부터 발생하는 에러 정보를 담는 멤버변수
+
+    private ErrorResponse(final List<FieldError> fieldErrors,
+                          final List<ConstraintViolationError> constraintViolationErrors) {
+        this.fieldErrors = fieldErrors;
+        this.constraintViolationErrors = constraintViolationErrors;
+    }
+
+    // MethodArgumentNotValidException 에 대한 ErrorResponse 객체 생성
+    public static ErrorResponse of(BindingResult bindingResult) {
+        return new ErrorResponse(FieldError.of(bindingResult), null);
+    }
+
+    // ConstraintViolationError 에 대한 ErrorResponse 객체 생성
+    public static ErrorResponse of(Set<ConstraintViolation<?>> violations) {
+        return new ErrorResponse(null, ConstraintViolationError.of(violations));
+    }
 
     @Getter
-    @AllArgsConstructor
     public static class FieldError {
         private String field;
         private Object rejectedValue;
         private String message;
+
+        private FieldError(String field, Object rejectedValue, String message) {
+            this.field = field;
+            this.rejectedValue = rejectedValue;
+            this.message = message;
+        }
+
+        public static List<FieldError> of(BindingResult bindingResult) {
+            final List<org.springframework.validation.FieldError> fieldErrors = bindingResult.getFieldErrors();
+            return fieldErrors.stream()
+                    .map(error -> new FieldError(
+                            error.getField(),
+                            error.getRejectedValue() == null ? "" : error.getRejectedValue().toString(),
+                            error.getDefaultMessage()))
+                    .collect(Collectors.toList());
+        }
+    }
+
+    @Getter
+    public static class ConstraintViolationError {
+        private String propertyPath;
+        private Object rejectedValue;
+        private String message;
+
+        private ConstraintViolationError(String propertyPath, Object rejectedValue, String message) {
+            this.propertyPath = propertyPath;
+            this.rejectedValue = rejectedValue;
+            this.message = message;
+        }
+
+        public static List<ConstraintViolationError> of(
+                Set<ConstraintViolation<?>> constraintViolations) {
+            return constraintViolations.stream()
+                    .map(constraintViolation -> new ConstraintViolationError(
+                            constraintViolation.getPropertyPath().toString(),
+                            constraintViolation.getInvalidValue().toString(),
+                            constraintViolation.getMessage()
+                    )).collect(Collectors.toList());
+        }
     }
 }
