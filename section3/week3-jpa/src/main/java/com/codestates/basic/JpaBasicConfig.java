58eb017d25d6fbfe63d608dc00a3f0aaf6011a6d
my-roki <anthonychangrok@gmail.com>
2022-11-02 11:49:07 +0900

    [Spring MVC] JPA 기반 데이터 액세스 계층 - 영속성 컨텍스트와 테이블에 엔티티 수정 및 삭제

--- section3/week3-jpa/src/main/java/com/codestates/basic/JpaBasicConfig.java
+++ section3/week3-jpa/src/main/java/com/codestates/basic/JpaBasicConfig.java
@@ -25,24 +25,19 @@ public class JpaBasicConfig {
     }
 
     private void example() {
-        tx.begin();  // Transaction을 시작하기 위해서 tx.begin() 메서드를 먼저 호출.
-        Member member1 = new Member("roki@hello.com");
-        Member member2 = new Member("toki@hello.com");
+        tx.begin();
+        Member createMember = new Member("roki@hello.com");
+        em.persist(createMember);  // member1 객체를 영속성 컨텍스트에 저장.
+        tx.commit();  // 지연 SQL 저장소에 등록된 INSERT 쿼리 실행.
 
-        em.persist(member1);  // member1 객체를 영속성 컨텍스트에 저장.
-        em.persist(member2);  // member2 객체를 영속성 컨텍스트에 저장.
+        tx.begin();
+        Member updateMember = em.find(Member.class, 1L);  // 테이블에 저장된 member 객체를 영속성 컨텍스트의 1차 캐시에서 조회
+        updateMember.setEmail("toki@hello.com");  // setter 메서드로 이메일 정보를 변경
+        tx.commit();  // 지연 SQL 저장소에 등록된 UPDATE 쿼리가 실행
 
-        tx.commit();  // 지연 SQL 저장소에 등록된 쿼리 실행.
-
-        // em에서 find를 호출하면 1차적으로 영속성 컨탣스트에 저장되어있는 객체를 먼저 조회합니다.
-        // 만약 영속성 컨텍스트 내에 객체가 존재하지 않는다면 쿼리를 통해 db에서 조회를 시도합니다.
-        Member resul1 = em.find(Member.class, 1L);
-        System.out.printf("id: %s%nemail: %s%n", resul1.getMemberId(), resul1.getEmail());
-
-        Member result2 = em.find(Member.class, 2L);
-        System.out.printf("id: %s%nemail: %s%n", result2.getMemberId(), result2.getEmail());
-
-        Member result3 = em.find(Member.class, 3L);
-        System.out.println(result3);
+        tx.begin();
+        Member deleteMember = em.find(Member.class, 1L);  // 테이블에 저장된 Member 클래스의 객체를 영속성 컨텍스트의 1차 캐시에서 조회
+        em.remove(deleteMember);  //  영속성 컨텍스트의 1차 캐시에 있는 엔티티를 제거를 요청
+        tx.commit();  // 영속성 컨텍스트의 1차 캐시에 있는 엔티티를 제거하고, 쓰기 지연 SQL 저장소에 등록된 DELETE 쿼리가 실행
     }
 }
