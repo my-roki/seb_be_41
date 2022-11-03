c019d8ef43e6efb7422ffc7e05bf37473d453bca
my-roki <anthonychangrok@gmail.com>
2022-11-03 13:25:05 +0900

    [Spring MVC] JPA 기반 데이터 액세스 계층 : Spring Data JPA - Update repository interface

---
+++ section3/week3-spring-data-jpa/src/main/java/com/codestates/coffee/repository/CoffeeRepository.java
@@ -0,0 +1,14 @@
+package com.codestates.coffee.repository;
+
+import com.codestates.coffee.entity.Coffee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface CoffeeRepository extends JpaRepository<Coffee, Long> {
+    Optional<Coffee> findByCoffeeCode(String coffeeCode);
+
+    @Query(value = "SELECT c FROM Coffee c WHERE c.coffeeId = :coffeeId")
+    Optional<Coffee> findByCoffee(long coffeeId);
+}
---
+++ section3/week3-spring-data-jpa/src/main/java/com/codestates/order/repository/OrderRepository.java
@@ -0,0 +1,6 @@
+package com.codestates.order.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository {
+}
