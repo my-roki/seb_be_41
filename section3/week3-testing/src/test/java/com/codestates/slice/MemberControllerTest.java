7a0f323f71b4fd09c7130ffbce4d85361b0620c8
my-roki <anthonychangrok@gmail.com>
2022-11-10 13:50:07 +0900

    [Spring MVC] 테스팅(Testing): Mockito - 슬라이스 테스트에 Mockito 적용

--- section3/week3-testing/src/test/java/com/codestates/slice/MemberControllerTest.java
+++ section3/week3-testing/src/test/java/com/codestates/slice/MemberControllerTest.java
@@ -1,16 +1,23 @@
 package com.codestates.slice;
 
 import com.codestates.member.dto.MemberDto;
+import com.codestates.member.entity.Member;
+import com.codestates.member.mapper.MemberMapper;
+import com.codestates.member.service.MemberService;
+import com.codestates.stamp.Stamp;
 import com.google.gson.Gson;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -24,10 +31,25 @@ public class MemberControllerTest {
     @Autowired
     private Gson gson;
 
+    // Mockito Mock Bean 생성
+    @MockBean
+    private MemberService memberService;
+
+    @Autowired
+    private MemberMapper mapper;
+
     @Test
     public void postMemberTest() throws Exception {
         // given
         MemberDto.Post post = new MemberDto.Post("roki@hello.com", "roki", "010-1234-5678");
+
+        Member member = mapper.memberPostToMember(post);
+        member.setStamp(new Stamp());
+
+        // Note. given()은 Mock 객체가 특정 값을 리턴하는 동작을 지정합니다.
+        // Note. createMember에서 Member 클래스를 받고 member를 리턴합니다.
+        given(memberService.createMember(Mockito.any(Member.class))).willReturn(member);
+
         String content = gson.toJson(post);
 
         // when
