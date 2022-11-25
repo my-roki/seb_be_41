7c6da5e62e65d5dddd586cfce19e167e35fdf394
my-roki <anthonychangrok@gmail.com>
2022-11-25 18:57:37 +0900

    [Spring Security] OAuth2 인증(Authentication) : OAuth2와 JWT를 이용한 샘플 애플리케이션 구현

--- section4/be-template-spring-security-oauth2-basic-main/src/main/java/com/codestates/hello_oauth2/config/SecurityConfiguration.java
+++ section4/be-template-spring-security-oauth2-basic-main/src/main/java/com/codestates/hello_oauth2/config/SecurityConfiguration.java
@@ -1,13 +1,24 @@
 package com.codestates.hello_oauth2.config;
 
+import com.codestates.member.service.MemberService;
+import com.codestates.oauth2_jwt.filter.JwtVerificationFilter;
+import com.codestates.oauth2_jwt.handler.MemberAccessDeniedHandler;
+import com.codestates.oauth2_jwt.handler.MemberAuthenticationEntryPoint;
+import com.codestates.oauth2_jwt.handler.OAuth2MemberSuccessHandler;
+import com.codestates.oauth2_jwt.jwt.JwtTokenizer;
+import com.codestates.oauth2_jwt.utils.CustomAuthorityUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -20,13 +31,45 @@ public class SecurityConfiguration {
     @Value("${spring.security.oauth2.client.registration.google.clientSecret}")
     private String clientSecret;
 
+    private final JwtTokenizer jwtTokenizer;
+    private final CustomAuthorityUtils authorityUtils;
+    private final MemberService memberService;
+
+    public SecurityConfiguration(JwtTokenizer jwtTokenizer, CustomAuthorityUtils authorityUtils, MemberService memberService) {
+        this.jwtTokenizer = jwtTokenizer;
+        this.authorityUtils = authorityUtils;
+        this.memberService = memberService;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf().disable()
+        httpSecurity
+                .headers().frameOptions().sameOrigin()
+                .and()
+                .csrf().disable()
+                .cors(withDefaults())
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 .formLogin().disable()
                 .httpBasic().disable()
-                .authorizeHttpRequests(a -> a.anyRequest().authenticated())
-                .oauth2Login(withDefaults());
+                .exceptionHandling()
+                .authenticationEntryPoint(new MemberAuthenticationEntryPoint())
+                .accessDeniedHandler(new MemberAccessDeniedHandler())
+                .and()
+                .apply(new CustomFilterConfigurer())
+                .and()
+                .authorizeHttpRequests(a -> a
+                        .antMatchers(HttpMethod.POST, "/*/coffees").hasRole("ADMIN")
+                        .antMatchers(HttpMethod.PATCH, "/*/coffees/**").hasRole("ADMIN")
+                        .antMatchers(HttpMethod.GET, "/*/coffees/**").hasAnyRole("USER", "ADMIN")
+                        .antMatchers(HttpMethod.GET, "/*/coffees").permitAll()
+                        .antMatchers(HttpMethod.DELETE, "/*/coffees").hasRole("ADMIN")
+                        .antMatchers(HttpMethod.POST, "/*/orders").hasRole("USER")
+                        .antMatchers(HttpMethod.PATCH, "/*/orders").hasAnyRole("USER", "ADMIN")
+                        .antMatchers(HttpMethod.GET, "/*/orders/**").hasAnyRole("USER", "ADMIN")
+                        .antMatchers(HttpMethod.DELETE, "/*/orders").hasRole("USER")
+                        .anyRequest().permitAll())
+                .oauth2Login(oauth2 -> oauth2.successHandler(new OAuth2MemberSuccessHandler(jwtTokenizer, authorityUtils, memberService)));
 
         return httpSecurity.build();
     }
@@ -45,5 +88,13 @@ public class SecurityConfiguration {
                 .clientId(clientId)
                 .clientSecret(clientSecret)
                 .build();
+    }
+
+    public class CustomFilterConfigurer extends AbstractHttpConfigurer<CustomFilterConfigurer, HttpSecurity> {
+        @Override
+        public void configure(HttpSecurity builder) {
+            JwtVerificationFilter jwtVerificationFilter = new JwtVerificationFilter(jwtTokenizer, authorityUtils);
+            builder.addFilterAfter(jwtVerificationFilter, OAuth2LoginAuthenticationFilter.class);
+        }
     }
 }
