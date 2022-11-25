2b620e0468dca934d507bf7a8a50a3e989ad8418
my-roki <anthonychangrok@gmail.com>
2022-11-25 15:14:01 +0900

    [Spring Security] OAuth2 인증(Authentication) : Configuration을 통한 OAuth 2 인증 설정

--- section4/be-template-spring-security-oauth2-basic-main/src/main/java/com/codestates/hello_oauth2/config/SecurityConfiguration.java
+++ section4/be-template-spring-security-oauth2-basic-main/src/main/java/com/codestates/hello_oauth2/config/SecurityConfiguration.java
@@ -1,14 +1,25 @@
 package com.codestates.hello_oauth2.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfiguration {
+    @Value("${spring.security.oauth2.client.registration.google.clientId}")
+    private String clientId;
+
+    @Value("${spring.security.oauth2.client.registration.google.clientSecret}")
+    private String clientSecret;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable()
@@ -18,5 +29,21 @@ public class SecurityConfiguration {
                 .oauth2Login(withDefaults());
 
         return httpSecurity.build();
+    }
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        ClientRegistration clientRegistration = clientRegistration();
+
+        return new InMemoryClientRegistrationRepository(clientRegistration);
+    }
+
+    private ClientRegistration clientRegistration() {
+        return CommonOAuth2Provider
+                .GOOGLE
+                .getBuilder("google")
+                .clientId(clientId)
+                .clientSecret(clientSecret)
+                .build();
     }
 }
