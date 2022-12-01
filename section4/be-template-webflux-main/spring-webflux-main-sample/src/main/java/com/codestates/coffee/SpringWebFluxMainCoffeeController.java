71f8bfa6f73352c02d67c9e3f26cf311c6f72ac3
my-roki <anthonychangrok@gmail.com>
2022-12-01 13:11:28 +0900

    [Spring WebFlux] Spring WebFlux : Spring WebFlux의 Non-Blocking 처리 방식

---
+++ section4/be-template-webflux-main/spring-webflux-main-sample/src/main/java/com/codestates/coffee/SpringWebFluxMainCoffeeController.java
@@ -0,0 +1,29 @@
+package com.codestates.coffee;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@RestController
+@RequestMapping("/v11/coffees")
+public class SpringWebFluxMainCoffeeController {
+    String uri = "http://localhost:5050/v11/coffees/1";
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("{coffee-id}")
+    public Mono<CoffeeResponseDto> getCoffee(@PathVariable("coffee-id") long coffeeId) {
+        log.info("# call Spring WebFlux Main Controller: {}", LocalDateTime.now());
+
+        // WebClient는 Non-Blocking 방식의 Rest Client
+        return WebClient.create()
+                .get()
+                .uri(uri)
+                .retrieve()
+                .bodyToMono(CoffeeResponseDto.class);
+    }
+}
