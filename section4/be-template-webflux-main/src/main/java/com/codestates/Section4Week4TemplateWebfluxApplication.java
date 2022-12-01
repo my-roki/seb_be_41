d330186bde60919eff461df9d95f6125338706b7
my-roki <anthonychangrok@gmail.com>
2022-12-01 14:19:16 +0900

    [Spring WebFlux] Spring WebFlux : 리액티브한 샘플 애플리케이션 구현 - 프로젝트 설정 및 공통 설정

--- section4/be-template-webflux-main/src/main/java/com/codestates/Section4Week4TemplateWebfluxApplication.java
+++ section4/be-template-webflux-main/src/main/java/com/codestates/Section4Week4TemplateWebfluxApplication.java
@@ -4,17 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
+@EnableR2dbcRepositories  // R2DBC Repository 사용
+@EnableR2dbcAuditing  // 공통 컬럼 자동 저장
 @SpringBootApplication
 public class Section4Week4TemplateWebfluxApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Section4Week4TemplateWebfluxApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Section4Week4TemplateWebfluxApplication.class, args);
+    }
 
 
-	@Bean
-	public RestTemplateBuilder restTemplateBuilder() {
-		return new RestTemplateBuilder();
-	}
+    @Bean
+    public RestTemplateBuilder restTemplateBuilder() {
+        return new RestTemplateBuilder();
+    }
 }
